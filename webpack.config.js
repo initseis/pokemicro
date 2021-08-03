2baea5e619119c69e44ccce03c96bfad9c010e45
eroauditore <alfa.fenrir@gmail.com>
2021-08-02 20:20:14 -0500

    pokemon container added

--- webpack.config.js
+++ webpack.config.js
@@ -1,29 +1,32 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
+  mode: "development",
   entry: {
-    index: './src/index.js',
+    index: "./src/index.js",
   },
   devServer: {
-    contentBase: './dist',
+    contentBase: "./dist",
   },
   plugins: [
     new HtmlWebpackPlugin({
-      title: 'Output Management',
-      template: './src/index.html',
+      title: "Output Management",
+      template: "./src/index.html",
     }),
   ],
   module: {
     rules: [
       {
         test: /\.css$/i,
-        use: ['style-loader', 'css-loader'],
+        use: ["style-loader", "css-loader"],
       },
     ],
   },
   output: {
-    filename: '[name].bundle.js',
-    path: path.resolve(__dirname, 'dist'),
+    filename: "[name].bundle.js",
+    celan: true,
+    path: path.resolve(__dirname, "dist"),
+    publicPath: "auto",
   },
 };
