8b311b2d024284fe1c187613ec7b3e77240b01ed
Kevin <kevinsevinche@gmail.com>
2021-08-03 09:59:03 -0500

    Add form event to add comments

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
+//import 'bootstrap/dist/css/bootstrap.min.css';
 
-const btnModal = document.querySelectorAll('button');
+const btnModal = document.querySelectorAll("button");
+const form = document.querySelector("#add-comment");
 
 const capitalize = (text) => {
   const lowercase = text.toLowerCase();
@@ -12,24 +13,27 @@ const displayComments = async (id) => {
     `https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/Vlgir6luyMApwO9KpUDb/comments?item_id=${id}`,
     {
       headers: {
-        'Content-type': 'application/json; charset=UTF-8',
+        "Content-type": "application/json; charset=UTF-8",
       },
-    },
+    }
   )
     .then((response) => response.json())
     .then((comments) => {
-      const commentsDiv = document.querySelector('#comments');
-      commentsDiv.innerHTML = '';
-      const commentsQuantity = document.createElement('h4');
+      const commentsDiv = document.querySelector("#comments");
+      commentsDiv.innerHTML = "";
+      const commentsQuantity = document.createElement("h4");
+      commentsQuantity.className = "text-sm-center";
       comments.length === undefined
-        ? (commentsQuantity.innerHTML = 'Comments (0)')
+        ? (commentsQuantity.innerHTML = "Comments (0)")
         : (commentsQuantity.innerHTML = `Comments (${comments.length})`);
       commentsDiv.appendChild(commentsQuantity);
-      comments.forEach((element) => {
-        const paragraph = document.createElement('p');
-        paragraph.innerHTML = `${element.creation_date} ${element.username}: ${element.comment}`;
-        commentsDiv.appendChild(paragraph);
-      });
+      if (comments.length !== undefined) {
+        comments.forEach((element) => {
+          const paragraph = document.createElement("p");
+          paragraph.innerHTML = `${element.creation_date} ${element.username}: ${element.comment}`;
+          commentsDiv.appendChild(paragraph);
+        });
+      }
     });
 };
 
@@ -38,44 +42,73 @@ const displayModal = async (event) => {
     `https://pokeapi.co/api/v2/pokemon/${event.target.id}`,
     {
       headers: {
-        'Content-type': 'application/json; charset=UTF-8',
+        "Content-type": "application/json; charset=UTF-8",
       },
-    },
+    }
   )
     .then((response) => response.json())
     .then((pokemon) => {
-      document.querySelector('#pokemon-name').innerHTML = pokemon.forms[0].name;
-      document.querySelector('#pokemon-img').src = pokemon.sprites.other.dream_world.front_default;
-      let types = 'Type:';
+      document.querySelector("#pokemon-name").innerHTML = capitalize(
+        pokemon.forms[0].name
+      );
+      document.querySelector("#pokemon-img").src =
+        pokemon.sprites.other.dream_world.front_default;
+      document.querySelector("#pokemon-img").width = "226";
+      document.querySelector("#pokemon-img").height = "218";
+      let types = "Type:";
       pokemon.types.map((element) => {
         types += `<span class="ms-1 badge bg-secondary">${element.type.name}</span>`;
       });
-      document.querySelector('#pokemon-type').innerHTML = types;
-      let abilities = 'Abilities: ';
+      document.querySelector("#pokemon-type").innerHTML = types;
+      let abilities = "Abilities: ";
       pokemon.abilities.map((element, i) => {
         pokemon.abilities.length === i + 1
           ? (abilities += capitalize(element.ability.name))
           : (abilities = `${abilities + capitalize(element.ability.name)}, `);
       });
-      document.querySelector('#pokemon-abilities').innerHTML = abilities;
+      document.querySelector("#pokemon-abilities").innerHTML = abilities;
       String(pokemon.height).length === 1
         ? (document.querySelector(
-          '#pokemon-height',
-        ).innerHTML = `Height: 0.${pokemon.height} mt`)
+            "#pokemon-height"
+          ).innerHTML = `Height: 0.${pokemon.height} mt`)
         : (document.querySelector(
-          '#pokemon-height',
-        ).innerHTML = `Height: ${Number(pokemon.height / 10)} mt`);
+            "#pokemon-height"
+          ).innerHTML = `Height: ${Number(pokemon.height / 10)} mt`);
       String(pokemon.weight).length === 1
         ? (document.querySelector(
-          '#pokemon-weight',
-        ).innerHTML = `Weight: 0.${pokemon.weight} kg`)
+            "#pokemon-weight"
+          ).innerHTML = `Weight: 0.${pokemon.weight} kg`)
         : (document.querySelector(
-          '#pokemon-weight',
-        ).innerHTML = `Weight: ${Number(pokemon.weight / 10)} kg`);
+            "#pokemon-weight"
+          ).innerHTML = `Weight: ${Number(pokemon.weight / 10)} kg`);
       displayComments(event.target.id);
+      form[2].value = event.target.id;
     });
 };
 
 btnModal.forEach((btn) => {
-  btn.addEventListener('click', displayModal);
+  btn.addEventListener("click", displayModal);
 });
+
+const addComment = async (event) => {
+  event.preventDefault();
+  const response = await fetch(
+    "https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/Vlgir6luyMApwO9KpUDb/comments/",
+    {
+      method: "POST",
+      body: JSON.stringify({
+        item_id: form[2].value,
+        username: form[0].value,
+        comment: form[1].value,
+      }),
+      headers: {
+        "Content-type": "application/json; charset=UTF-8",
+      },
+    }
+  ).then(() => {
+    displayComments(form[2].value);
+  });
+  console.log(event.target);
+};
+
+form.addEventListener("submit", addComment);
