4f678f6eaf9e7fba6a49a2b557a7d0e359aec669
eroauditore <alfa.fenrir@gmail.com>
2021-08-03 12:53:07 -0500

    Fetch api and append cards to DOM

--- src/home/home.js
+++ src/home/home.js
@@ -1,13 +1,16 @@
 import { getPokemons } from './api';
+import { pokemonCard } from './components/pokemonCard';
 
 const initializeHome = () => {
+  const pokemonContainer = document.getElementById('pokemon-container');
   const result = getPokemons();
   result
     .then((response) => response.json())
     .then(({ results }) => {
       results.forEach((pokemon, index) => {
         pokemon.id = index + 1;
-        console.log(pokemon);
+        pokemonContainer.insertAdjacentHTML('beforeend', pokemonCard(pokemon));
+        //console.log(pokemon);
       });
     });
 };
