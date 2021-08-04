a025430c7a8fb1963344deaa9ddfab88c2c1da0d
eroauditore <alfa.fenrir@gmail.com>
2021-08-04 14:11:55 -0500

    Save like to API

--- src/home/api.js
+++ src/home/api.js
@@ -1,5 +1,8 @@
 const pokeURL = 'https://pokeapi.co/api/v2/pokemon?limit=15&offset=0';
-const pokeLikeURL = 'https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/Hh95XrOFts64CIu0nZBU/likes';
+const pokeLikeURL =
+  'https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/Hh95XrOFts64CIu0nZBU/likes';
+
+let dataLikes = [];
 
 const getPokemons = async () => {
   const result = fetch(pokeURL);
@@ -12,7 +15,31 @@ const getLikes = async () => {
 };
 
 const saveLike = async (pokemon) => {
-  const result = fetch(pokeLikeURL);
-  return result;
+  //const result = fetch(pokeLikeURL);
+
+  const response = await fetch(pokeLikeURL, {
+    method: 'POST', // *GET, POST, PUT, DELETE, etc.
+    //mode: 'cors', // no-cors, *cors, same-origin
+    //cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+    //credentials: 'same-origin', // include, *same-origin, omit
+    headers: {
+      'Content-Type': 'application/json',
+      // 'Content-Type': 'application/x-www-form-urlencoded',
+    },
+    //redirect: 'follow', // manual, *follow, error
+    //referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+    body: JSON.stringify({ item_id: pokemon }), // body data type must match "Content-Type" header
+  });
+  return response;
 };
-export { getPokemons, getLikes, saveLike };
+
+const getDataLikes = () => {
+  console.log('get dataLikes', dataLikes);
+  return dataLikes;
+};
+
+const setDataLikes = (likes) => {
+  dataLikes = [...likes];
+};
+
+export { getPokemons, getLikes, saveLike, getDataLikes, setDataLikes };
--- src/home/home.js
+++ src/home/home.js
@@ -1,5 +1,19 @@
-import { getPokemons, getLikes } from './api';
+import {
+  getPokemons,
+  getLikes,
+  getDataLikes,
+  setDataLikes,
+  saveLike,
+} from './api';
 import pokemonCard from './components/pokemonCard';
+
+const addLike = (e) => {
+  console.log('data likes', getDataLikes());
+  console.log(e.target.getAttribute('data-pokemon'));
+  const pokemonLike = e.target.getAttribute('data-pokemon');
+  const result = saveLike(pokemonLike);
+  result.then((response) => console.log('saved like'));
+};
 
 const initializeHome = () => {
   const pokemonContainer = document.getElementById('pokemon-container');
@@ -8,23 +22,25 @@ const initializeHome = () => {
   likes
     .then((response) => response.json())
     .then((likes) => {
-      // console.log(likes);
-
       const result = getPokemons();
       result
         .then((response) => response.json())
         .then(({ results }) => {
+          setDataLikes(likes);
           results.forEach((pokemon, index) => {
             const pokemonLike = likes.find(
-              (element) => element.item_id === pokemon.name,
+              (element) => element.item_id === pokemon.name
             );
             pokemon.id = index + 1;
             pokemon.likes = pokemonLike ? pokemonLike.likes : '';
-            console.log(pokemon);
             pokemonContainer.insertAdjacentHTML(
               'beforeend',
-              pokemonCard(pokemon),
+              pokemonCard(pokemon)
             );
+            const likeBtn = document.querySelectorAll('#btnLike');
+            likeBtn.forEach((btn) => {
+              btn.addEventListener('click', addLike);
+            });
           });
         });
     });
