1e6b88153ddf7988da21ff80a95fbe36a66378e5
Kevin <kevinsevinche@gmail.com>
2021-08-04 10:24:34 -0500

    Fix Linter Errors

--- comments.test.js
+++ comments.test.js
@@ -1,36 +1,35 @@
 /**
  * @jest-environment jsdom
  */
-import { commentsCounter } from "./src/js-scripts/comments.js";
+import { commentsCounter } from './src/js-scripts/comments.js';
 
-describe("Testing the comments counter function", () => {
-  test("Count three comments", () => {
-    //Arrange
-    document.body.innerHTML =
-      "<div id='commentsWrapper'>" +
-      "  <p>2021-08-01 Jane: Nice Pokemon!</p>" +
-      "  <p>2021-08-02 John: My fav <3</p>" +
-      "  <p>2021-08-03 Kevin: GOAT</p>" +
-      "</div>";
+describe('Testing the comments counter function', () => {
+  test('Count three comments', () => {
+    // Arrange
+    document.body.innerHTML = "<div id='commentsWrapper'>"
+      + '  <p>2021-08-01 Jane: Nice Pokemon!</p>'
+      + '  <p>2021-08-02 John: My fav <3</p>'
+      + '  <p>2021-08-03 Kevin: GOAT</p>'
+      + '</div>';
 
-    //Act
-    const comments = commentsCounter(document.querySelector("div"));
+    // Act
+    const comments = commentsCounter(document.querySelector('div'));
 
-    //Assert
+    // Assert
     expect(comments).toBe(3);
   });
 
-  test("Add a comment and count the comments", () => {
-    //Arrange
-    const newComment = document.createElement("p");
-    newComment.innerHTML = "2021-08-04 Lu: Lovely";
-    const divWrapComments = document.querySelector("div");
+  test('Add a comment and count the comments', () => {
+    // Arrange
+    const newComment = document.createElement('p');
+    newComment.innerHTML = '2021-08-04 Lu: Lovely';
+    const divWrapComments = document.querySelector('div');
     divWrapComments.appendChild(newComment);
 
-    //Act
+    // Act
     const comments = commentsCounter(divWrapComments);
 
-    //Assert
+    // Assert
     expect(comments).toBe(4);
   });
 });
--- src/js-scripts/comments.js
+++ src/js-scripts/comments.js
@@ -1,29 +1,31 @@
-import { getPokemonComments, setPokemonComment } from "./API.js";
-import { blankInputs } from "./strings.js";
+import { getPokemonComments, setPokemonComment } from './API.js';
+import { blankInputs } from './strings.js';
 
-const form = document.querySelector("#add-comment");
+const form = document.querySelector('#add-comment');
+
+const commentsCounter = (div) => div.childElementCount;
 
 const displayComments = async (id) => {
   const response = getPokemonComments(id);
   response.then((comments) => {
-    const commentsDiv = document.querySelector("#comments");
-    commentsDiv.innerHTML = "";
-    const commentsQuantity = document.createElement("h5");
-    commentsQuantity.className = "text-sm-center mt-2";
+    const commentsDiv = document.querySelector('#comments');
+    commentsDiv.innerHTML = '';
+    const commentsQuantity = document.createElement('h5');
+    commentsQuantity.className = 'text-sm-center mt-2';
     commentsDiv.appendChild(commentsQuantity);
-    const divWrapComments = document.createElement("div");
-    divWrapComments.className = "d-flex flex-column align-items-sm-start ps-4";
+    const divWrapComments = document.createElement('div');
+    divWrapComments.className = 'd-flex flex-column align-items-sm-start ps-4';
     commentsDiv.appendChild(divWrapComments);
     if (comments.length !== undefined) {
       comments.forEach((element) => {
-        const paragraph = document.createElement("p");
-        paragraph.className = "m-0";
+        const paragraph = document.createElement('p');
+        paragraph.className = 'm-0';
         paragraph.innerHTML = `${element.creation_date} ${element.username}: ${element.comment}`;
         divWrapComments.appendChild(paragraph);
       });
     }
     commentsQuantity.innerHTML = `Comments (${commentsCounter(
-      divWrapComments
+      divWrapComments,
     )})`;
   });
 };
@@ -33,14 +35,12 @@ const addComment = async (event) => {
   const response = setPokemonComment(
     form[2].value,
     form[0].value,
-    form[1].value
+    form[1].value,
   );
   response.then(() => {
     displayComments(form[2].value);
     blankInputs();
   });
 };
-
-const commentsCounter = (div) => div.childElementCount;
 
 export { displayComments, addComment, commentsCounter };
