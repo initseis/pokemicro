ffcc03f767a3e58bac96c678c2440a67dc94e822
Kevin <kevinsevinche@gmail.com>
2021-08-05 11:29:35 -0500

    Update README file and build the project with Webpack

--- src/home/api.js
+++ src/home/api.js
@@ -1,6 +1,6 @@
-const pokeURL = 'https://pokeapi.co/api/v2/pokemon?limit=25&offset=0';
+const pokeURL = "https://pokeapi.co/api/v2/pokemon?limit=150&offset=0";
 const pokeLikeURL =
-  'https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/Hh95XrOFts64CIu0nZBU/likes';
+  "https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/Hh95XrOFts64CIu0nZBU/likes";
 
 let dataLikes = [];
 
@@ -16,9 +16,9 @@ const getLikes = async () => {
 
 const saveLike = async (pokemon) => {
   const response = await fetch(pokeLikeURL, {
-    method: 'POST', // *GET, POST, PUT, DELETE, etc.
+    method: "POST", // *GET, POST, PUT, DELETE, etc.
     headers: {
-      'Content-Type': 'application/json',
+      "Content-Type": "application/json",
       // 'Content-Type': 'application/x-www-form-urlencoded',
     },
     body: JSON.stringify({ item_id: pokemon }), // body data type must match "Content-Type" header
@@ -34,8 +34,8 @@ const setDataLikes = (likes) => {
 
 const updatePokemonLike = (pokemon) => {
   const findObj = dataLikes.find((obj) => obj.item_id === pokemon);
-  if (typeof findObj === 'undefined') {
-    console.log('push', dataLikes);
+  if (typeof findObj === "undefined") {
+    console.log("push", dataLikes);
     dataLikes.push({
       likes: 1,
       item_id: pokemon,
--- src/home/components/pokemonCard.js
+++ src/home/components/pokemonCard.js
@@ -1,31 +1,36 @@
+import { capitalize } from "../../js-scripts/strings.js";
+
 const pokemonCard = (pokemon) => {
-  const card = ''
-    + '<div class="col-xl-3 col-md-4 col-xs-12 p-2">'
-    + '          <div class="card pt-3">'
-    + '            <img'
-    + `              src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${pokemon.id}.svg"`
-    + '              alt="pokemon"'
-    + '              class="card-img-top img-pokemon"'
-    + '            />'
-    + '            <div class="card-body">'
-    + '              <div class="row">'
-    + '                <div class="col-8">'
-    + `                  <h5 class="card-title">${pokemon.name}</h5>`
-    + '                </div>'
-    + '                <div class="col-4 d-flex justify-content-center">'
-    + `                  <a href="#" ><i id="btnLike" data-pokemon="${pokemon.name}" class="fas fa-heart"></i></a>`
-    + `                  <p class="ms-2" id="like-${pokemon.name}">${pokemon.likes}</p>`
-    + '                </div>'
-    + '              </div>'
-    + '              <div class="row">'
-    + '                <div class="col-12">'
-    + `                  <button class="btn btn-primary w-100" id="${pokemon.name}" data-bs-toggle="modal" data-bs-target="#pokemonModal">Comments</button>`
-    + '                </div>'
-    + '              </div>'
-    + '            </div>'
-    + '          </div>'
-    + '        </div>'
-    + '';
+  const card =
+    "" +
+    '<div class="col-xl-3 col-md-4 col-xs-12 p-2">' +
+    '          <div class="card pt-3">' +
+    "            <img" +
+    `              src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${pokemon.id}.svg"` +
+    '              alt="pokemon"' +
+    '              class="card-img-top img-pokemon"' +
+    "            />" +
+    '            <div class="card-body">' +
+    '              <div class="row">' +
+    '                <div class="col-8">' +
+    `                  <h5 class="card-title">${capitalize(
+      pokemon.name
+    )}</h5>` +
+    "                </div>" +
+    '                <div class="col-4 d-flex justify-content-center">' +
+    `                  <a href="#" ><i id="btnLike" data-pokemon="${pokemon.name}" class="fas fa-heart"></i></a>` +
+    `                  <p class="ms-2 hearts" id="like-${pokemon.name}">${pokemon.likes}</p>` +
+    "                </div>" +
+    "              </div>" +
+    '              <div class="row">' +
+    '                <div class="col-12">' +
+    `                  <button class="btn w-100" id="${pokemon.name}" data-bs-toggle="modal" data-bs-target="#pokemonModal">Comments</button>` +
+    "                </div>" +
+    "              </div>" +
+    "            </div>" +
+    "          </div>" +
+    "        </div>" +
+    "";
 
   return card;
 };
